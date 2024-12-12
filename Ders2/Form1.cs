b5c6355f618396681a79139b79af884869639cd9
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-12 22:52:02 +0300

    Ders2

---
+++ Ders2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Mehmet Burçhan";
+            label5.Text = "Gürses";
+            label6.Text = "Yazılım Geliştirici";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label7.Text = textBox1.Text;
+        }
+    }
+}
