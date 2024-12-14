5a15b70c34f615c9fdcf24f951201dc4fde2c5df
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-15 01:19:10 +0300

    Ders19

--- Ders19/Form1.Designer.cs
+++ Ders19/Form1.Designer.cs
@@ -224,6 +224,7 @@
             this.Controls.Add(this.BtnA);
             this.Controls.Add(this.richTextBox1);
             this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Bilgi Yarışması";
