a10d3573758cf3667dc61854e2220640971dce96
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-16 02:58:32 +0300

    Ders30

---
+++ Ders30/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = { 4, 7, 5, 6, 9, 8, 2, 3 };
+            label1.Text = sayilar[5].ToString();
+            
+        }
+    }
+}
