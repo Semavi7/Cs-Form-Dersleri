a65f96711efe3c3bacce9beb52b777480746772a
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-15 20:56:41 +0300

    Ders26

---
+++ Ders26/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders26
+{
+    public partial class Form1 : Form
+    {
+        int sayac = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+            if (sayac == 2)
+            {
+                this.BackColor = Color.Red;
+            }
+            if (sayac == 4)
+            {
+                this.BackColor = Color.Green;
+                sayac = 0;
+            }
+        }
+    }
+}
