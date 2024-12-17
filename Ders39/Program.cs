d89ffa22c33b898f9178420cfa6575b9f0274b62
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-17 21:58:21 +0300

    Ders39

---
+++ Ders39/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaj msj = new Mesaj();
+            msj.metin();
+        }
+    }
+}
