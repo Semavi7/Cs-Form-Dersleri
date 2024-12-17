6a93c6366febb4532413a34ccf61111898faaba0
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-17 21:27:11 +0300

    Ders36

---
+++ Ders36/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders36
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form1 = new Form1();
+            Form2 form2 = new Form2();
+            form1.Show();
+            form2.Show();
+            Application.Run();
+        }
+    }
+}
