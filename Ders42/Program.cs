51218b6984f691f7da12f1b050f6d6937a02ebb2
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-17 22:16:35 +0300

    Ders42

---
+++ Ders42/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders42
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string blg;
+            Console.Write("Ad - Kulüp: ");
+            blg = Console.ReadLine();
+            Ogrenci org = new Ogrenci(blg);
+        }
+    }
+}
