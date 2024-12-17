6bc05cbbfa86029798d3cf4b64c52e9bd4916c39
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-17 22:10:48 +0300

    Ders41

---
+++ Ders41/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            islem isl = new islem();
+            isl.topla(4, 6);
+            Console.WriteLine("\n");
+            isl.kare(5);
+        }
+    }
+}
---
+++ Ders41/islem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders41
+{
+    internal class islem
+    {
+        public int topla(int s1, int s2)
+        {
+            int s3 = s1 + s2;
+            Console.WriteLine("Sonuc: " + s3);
+            return s3;
+        }
+        public int kare(int sayi)
+        {
+            int sonuc = sayi * sayi;
+            Console.WriteLine("Sonuc: " + sonuc);
+            return sonuc;
+        }
+    }
+}
