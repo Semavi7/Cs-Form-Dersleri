1a38309e9a76f44222bf7d62d5203cbe39f9cbd6
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-17 22:03:31 +0300

    Ders40

---
+++ Ders40/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders40
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisiler ks = new Kisiler();
+            string ads;
+            ads = Console.ReadLine();
+            ks.kisilistesi(ads);
+        }
+    }
+}
