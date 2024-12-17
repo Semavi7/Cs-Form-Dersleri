36b68beaf2a6ef450ea8eff38ad2a4a1df2d8528
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-18 00:36:06 +0300

    Ders43

---
+++ Ders43/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kimlik kml = new kimlik();
+            kml.AD = "Murat";
+            kml.SOYAD = "Yücedağ";
+            kml.MEMLEKET = "Elazığ";
+
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.YAS);
+            Console.WriteLine(kml.CINSIYET);
+        }
+    }
+}
