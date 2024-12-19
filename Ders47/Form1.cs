7ec6cc716f81d0a1d51a1f7ddeb7481fa5d6d6d5
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-19 21:50:09 +0300

    Ders47

---
+++ Ders47/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders47
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            int s1, s2, s3;
+            s1 = rd.Next(1, 5);
+            s2 = rd.Next(1, 5);
+            s3 = rd.Next(1, 5);
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();
+            label3.Text = s3.ToString();
+        }
+    }
+}
