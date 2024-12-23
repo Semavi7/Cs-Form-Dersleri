89014a6981e3d9542e0db01897bac5c34d5da508
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-24 01:09:38 +0300

    Ders 62 Hastane Projesi

--- Ders62/FrmGirisler.Designer.cs
+++ Ders62/FrmGirisler.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ders62
 {
-    partial class Form1
+    partial class FrmGirisler
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGirisler));
             this.BtnHasta = new System.Windows.Forms.Button();
             this.BtnDoktor = new System.Windows.Forms.Button();
             this.BtnSekreter = new System.Windows.Forms.Button();
