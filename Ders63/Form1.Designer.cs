94d8305416a50e010a40ff9f501dd47e93407d31
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-28 21:34:28 +0300

    Ders63 Kitaplık Projesi

--- Ders63/Form1.Designer.cs
+++ Ders63/Form1.Designer.cs
@@ -51,12 +51,13 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(9, 241);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(550, 245);
+            this.dataGridView1.Size = new System.Drawing.Size(800, 245);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
@@ -64,43 +65,43 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(18, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.Size = new System.Drawing.Size(75, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "Kitap Id :";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 61);
+            this.label2.Location = new System.Drawing.Point(14, 61);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.Size = new System.Drawing.Size(79, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Kitap Ad :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 97);
+            this.label3.Location = new System.Drawing.Point(35, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 24);
+            this.label3.Size = new System.Drawing.Size(58, 18);
             this.label3.TabIndex = 3;
             this.label3.Text = "Yazar :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(42, 133);
+            this.label4.Location = new System.Drawing.Point(37, 133);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(69, 24);
+            this.label4.Size = new System.Drawing.Size(56, 18);
             this.label4.TabIndex = 4;
             this.label4.Text = "Sayfa :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(58, 169);
+            this.label5.Location = new System.Drawing.Point(49, 169);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 24);
+            this.label5.Size = new System.Drawing.Size(44, 18);
             this.label5.TabIndex = 5;
             this.label5.Text = "Tür :";
             // 
@@ -109,52 +110,52 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(30, 205);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(81, 24);
+            this.label6.Size = new System.Drawing.Size(63, 18);
             this.label6.TabIndex = 6;
             this.label6.Text = "Durum:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 25);
+            this.textBox1.Location = new System.Drawing.Point(99, 25);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 30);
+            this.textBox1.Size = new System.Drawing.Size(211, 26);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(117, 61);
+            this.textBox2.Location = new System.Drawing.Point(99, 61);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(211, 30);
+            this.textBox2.Size = new System.Drawing.Size(211, 26);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(117, 97);
+            this.textBox3.Location = new System.Drawing.Point(99, 97);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(211, 30);
+            this.textBox3.Size = new System.Drawing.Size(211, 26);
             this.textBox3.TabIndex = 9;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(117, 133);
+            this.textBox4.Location = new System.Drawing.Point(99, 133);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(211, 30);
+            this.textBox4.Size = new System.Drawing.Size(211, 26);
             this.textBox4.TabIndex = 10;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(117, 169);
+            this.comboBox1.Location = new System.Drawing.Point(99, 169);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(211, 32);
+            this.comboBox1.Size = new System.Drawing.Size(211, 26);
             this.comboBox1.TabIndex = 11;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(117, 207);
+            this.radioButton1.Location = new System.Drawing.Point(99, 205);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(136, 28);
+            this.radioButton1.Size = new System.Drawing.Size(104, 22);
             this.radioButton1.TabIndex = 12;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Kullanılmış";
@@ -163,9 +164,9 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(259, 207);
+            this.radioButton2.Location = new System.Drawing.Point(209, 205);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(69, 28);
+            this.radioButton2.Size = new System.Drawing.Size(55, 22);
             this.radioButton2.TabIndex = 13;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "Sıfır";
@@ -209,9 +210,10 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 498);
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(821, 498);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -231,9 +233,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Ders63/Form1.cs
+++ Ders63/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Ders63
 {
@@ -15,6 +16,20 @@ namespace Ders63
         public Form1()
         {
             InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\semav\\OneDrive\\Desktop\\Kitaplık.mdb");
+
+        void listele()
+        {
+            DataTable dt = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * From Kitaplar", baglanti);
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
         }
     }
 }
