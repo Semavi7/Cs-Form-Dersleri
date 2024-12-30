8ba4322a1724e7c14fb336eaf8af55cc704f0e44
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2024-12-30 22:51:13 +0300

    Ders65

---
+++ Ders65/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders65
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sayi1 = Convert.ToInt16(textBox1.Text);
+                int sayi2 = Convert.ToInt16(textBox2.Text);
+                int toplam = sayi1 * sayi2;
+                label3.Text = toplam.ToString();
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.ToString());
+            }
+            finally
+            {
+                MessageBox.Show("Finally Kodu Çalıştı");
+            }
+        }
+    }
+}
